af673fb39effdf4fe65cfae3667d6138ddb93035
Nandha <nandhagam@live.com>
2021-11-22 07:15:15 +0530

    initial commit

---
+++ Workouts/Task-4(user-binding)/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { ContentModule } from './content/content.module';
+import { AppComponent } from './app.component';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    AuthenticationModule,
+    ContentModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Workouts/Task-4(user-binding)/src/app/content/content.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContentRoutingModule } from './content-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ContentComponent } from './content.component';
+
+
+@NgModule({
+  declarations: [
+    WelcomeComponent,
+    ContentComponent
+  ],
+  imports: [
+    CommonModule,
+    ContentRoutingModule
+
+  ],
+  exports: [ContentRoutingModule]
+
+})
+export class ContentModule { }
