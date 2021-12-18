183e1302d60eb2af0064e1e2b4f6a4f963ef39f3
prithivirajsr <kingprithiviraj27@gmail.com>
2021-12-18 15:59:03 +0530

    all changes implemented and taskid added newly to array

--- Workouts/Task-4(user-binding)/src/app/authentication/login/login.component.ts
+++ Workouts/Task-4(user-binding)/src/app/authentication/login/login.component.ts
@@ -1,26 +1,24 @@
-import {Component, ElementRef, EventEmitter, Output, ViewChild} from "@angular/core";
-import {Router, ActivatedRoute} from "@angular/router";
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent {
-  constructor(private router: Router, private activatedroute: ActivatedRoute) {
-  }
+    constructor(private router: Router, private activatedroute: ActivatedRoute) {
+    }
+    storeValue: Array<any>;
+    firstname: string;
+    lastname: string;
+    counter: number = 0;
+    store: object;
+    @ViewChild("email") Email: ElementRef;
+    @ViewChild("password") Password: ElementRef;
 
-  storeValue: Array<any>;
-  firstname: string;
-  lastname: string;
-  counter: number = 0;
-  store: object;
-  @ViewChild("email") Email: ElementRef;
-  @ViewChild("password") Password: ElementRef;
 
-
-<<<<<<< HEAD
     submitValue = ($event: any) => {
         this.storeValue = JSON.parse(localStorage.getItem("userDetails")) || [];
         for (let x in this.storeValue) {
@@ -47,38 +45,6 @@ export class LoginComponent {
             alert("Entered email does not exist or check your credentials");
             this.counter = 0;
         }
-=======
-  submitValue = ($event: any) => {
-    this.storeValue = JSON.parse(localStorage.getItem("userDetails") || '[]');
-    for (let x in this.storeValue) {
-      if (this.Email.nativeElement.value == this.storeValue[x].email &&
-        this.Password.nativeElement.value == this.storeValue[x].password) {
-        this.store = {
-          firstname: this.storeValue[x].firstname,
-          lastname: this.storeValue[x].lastname,
-          email: this.storeValue[x].email,
-          password: this.storeValue[x].password
-        };
-        sessionStorage.setItem("userDetails", JSON.stringify(this.store));
-        this.router.navigate(['welcome'], {
-          queryParams: {
-            firstname: this.storeValue[x].firstname,
-            lastname: this.storeValue[x].lastname
-          }
-        });
-        return;
-      }
     }
-    for (let x in this.storeValue) {
-      if (this.Email.nativeElement.value != this.storeValue[x].email || this.Password.nativeElement.value != this.storeValue[x].password) {
-        this.counter = this.counter + 1;
-      }
-    }
-    if (this.storeValue.length == this.counter) {
-      alert("Entered email does not exist");
-      this.counter = 0;
->>>>>>> 195ec61497168d3beb4d6fa23a4fa8ff0e939512
-    }
-  }
 
 }
--- Workouts/Task-4(user-binding)/src/app/content/tasklist/tasklist.component.ts
+++ Workouts/Task-4(user-binding)/src/app/content/tasklist/tasklist.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
-import {TaskService} from '../task.service';
-import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import { TaskService } from '../task.service';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -16,7 +16,6 @@ export class TasklistComponent implements OnInit {
   today = moment();
   todayDate = this.today.format("MM-DD-YYYY");
   resultArray: Array<any> = [];
-
   constructor(public taskservice: TaskService) {
   }
 
@@ -27,38 +26,24 @@ export class TasklistComponent implements OnInit {
     this.taskservice.taskSubscription.subscribe((data: Array<any>) => {
       this.DisplayTask = data;
       this.displayDefault();
-<<<<<<< HEAD
 
     })
 
 
-  }
-
-  displayDefault() {
-=======
-    })
   }
 
   displayDefault() {
     this.resultArray = [];
->>>>>>> 195ec61497168d3beb4d6fa23a4fa8ff0e939512
     this.DisplayTask.forEach((task, i) => {
       let result: boolean;
       result = moment(this.DisplayTask[i].date).isSame(this.todayDate);
       if (result == true) {
         this.resultArray.push(this.DisplayTask[i]);
       }
-<<<<<<< HEAD
 
     })
   }
 
-=======
-    })
-  }
-
-
->>>>>>> 195ec61497168d3beb4d6fa23a4fa8ff0e939512
   checkBoxValue(event, index) {
     this.DisplayTask[index].taskstatus == event;
     localStorage.setItem("tasklist", JSON.stringify(this.DisplayTask));
@@ -73,6 +58,8 @@ export class TasklistComponent implements OnInit {
           result = moment(this.DisplayTask[i].date).isBefore(this.todayDate);
           if (result == true) {
             this.resultArray.push(this.DisplayTask[i]);
+
+
           }
         })
         break;
@@ -102,17 +89,10 @@ export class TasklistComponent implements OnInit {
   }
 
 
-<<<<<<< HEAD
   deleteTask(resultArray, taskId) {
     let index = this.DisplayTask.findIndex(resultArray => resultArray.taskId == taskId);
     this.DisplayTask.splice(index, 1);
-=======
-  deleteTask(index) {
-   /* this.DisplayTask.splice(index, 1);*/
-    // task.id => findIndex => splice
-
-    // _.remove
->>>>>>> 195ec61497168d3beb4d6fa23a4fa8ff0e939512
     localStorage.setItem("tasklist", JSON.stringify(this.DisplayTask));
+    this.displayDefault();
   }
 }
