dbf3a4572b644859272abbf27b7c01a46d737e0b
prithivirajsr <kingprithiviraj27@gmail.com>
2021-12-22 18:36:12 +0530

    all errors corrected in pipe and tasklistcomponent

--- Workouts/Task-4(user-binding)/src/app/content/tasklist/tasklist.component.ts
+++ Workouts/Task-4(user-binding)/src/app/content/tasklist/tasklist.component.ts
@@ -13,10 +13,7 @@ export class TasklistComponent implements OnInit {
 
   DisplayTask: Array<any> = [];
   faTrash = faTrash;
-  today = moment();
-  todayDate = this.today.format("YYYY MM DD");
-  resultArray: Array<any> = [];
-  /*  filterCriteria = 0; */
+  filterCriteria = 0; //due to filterCriteria is 0 it will display only today task in the view
 
   constructor(public taskservice: TaskService) {
   }
@@ -24,81 +21,23 @@ export class TasklistComponent implements OnInit {
 
   ngOnInit() {
     this.DisplayTask = this.taskservice.displayTask || [];
-    this.displayDefault();
-    /* this.filterTask(this.filterCriteria); */
     this.taskservice.taskSubscription.subscribe((data: Array<any>) => {
       this.DisplayTask = data;
-      this.displayDefault();
-      /*  this.filterTask(this.filterCriteria); */
-
-    })
-
-
-  }
-
-  displayDefault() {
-    this.resultArray = [];
-    this.DisplayTask.forEach((task, i) => {
-      let result: boolean;
-      result = moment(this.DisplayTask[i].date).isSame(this.todayDate);
-      if (result == true) {
-        this.resultArray.push(this.DisplayTask[i]);
-      }
-
     })
   }
+
+
 
   checkBoxValue(event, index) {
     this.DisplayTask[index].taskstatus == event;
     localStorage.setItem("tasklist", JSON.stringify(this.DisplayTask));
   }
 
-  /* filterTask(criteria: any) {
-    this.filterCriteria = criteria;
-    let result: boolean;
-    this.resultArray = [];
-    switch (criteria) {
-      case -1: {
-        this.DisplayTask.forEach((task, i) => {
-          result = moment(this.DisplayTask[i].date).isBefore(this.todayDate);
-          if (result == true) {
-            this.resultArray.push(this.DisplayTask[i]);
-
-
-          }
-        })
-        break;
-      }
-      case 0: {
-        this.DisplayTask.forEach((task, i) => {
-          result = moment(this.DisplayTask[i].date).isSame(this.todayDate);
-          if (result == true) {
-            this.resultArray.push(this.DisplayTask[i]);
-          }
-        })
-        break;
-      }
-      case 1: {
-        this.DisplayTask.forEach((task, i) => {
-          result = moment(this.DisplayTask[i].date).isAfter(this.todayDate);
-          if (result == true) {
-            this.resultArray.push(this.DisplayTask[i]);
-          }
-        })
-        break;
-      }
-      case 2: {
-        this.resultArray = this.DisplayTask;
-      }
-    }
-  }
 
 
   deleteTask(taskId) {
     let index = this.DisplayTask.findIndex(taskObj => taskObj.taskId == taskId);
     this.DisplayTask.splice(index, 1);
     localStorage.setItem("tasklist", JSON.stringify(this.DisplayTask));
-    /*  this.displayDefault(); */
-  /*  this.filterTask(this.filterCriteria);
- } */
+  }
 }
--- Workouts/Task-4(user-binding)/src/app/sharedmodule/pipes/custom.pipe.ts
+++ Workouts/Task-4(user-binding)/src/app/sharedmodule/pipes/custom.pipe.ts
@@ -2,70 +2,53 @@ import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
 
 @Pipe({
-  name: 'custom'
+  name: 'custom',
+  pure: false
 })
 export class CustomPipe implements PipeTransform {
 
-  filterCriteria = 0;
-  DisplayTask: Array<any> = [];
-  resultArray: Array<any> = [];
-  today = moment();
-  todayDate = this.today.format("YYYY MM DD");
 
-  transform(value: unknown, ...args: unknown[]): unknown {
 
-    filterTask(criteria: any) {
-      this.filterCriteria = criteria;
-      let result: boolean;
-      this.resultArray = [];
-      switch (criteria) {
+  transform(value: Array<any>, arg: number): Array<any> {
+
+    const filterTask = () => {
+      let resultArray = [];
+      const todayDate = moment().format("YYYY MM DD");
+      switch (arg) {
         case -1: {
-          this.DisplayTask.forEach((task, i) => {
-            result = moment(this.DisplayTask[i].date).isBefore(this.todayDate);
-            if (result == true) {
-              this.resultArray.push(this.DisplayTask[i]);
-
-
+          value.forEach((task, i) => {
+            if (moment(value[i].date).isBefore(todayDate) == true) {
+              resultArray.push(value[i]);
             }
           })
           break;
         }
         case 0: {
-          this.DisplayTask.forEach((task, i) => {
-            result = moment(this.DisplayTask[i].date).isSame(this.todayDate);
-            if (result == true) {
-              this.resultArray.push(this.DisplayTask[i]);
+          value.forEach((task, i) => {
+            if (moment(value[i].date).isSame(todayDate) == true) {
+              resultArray.push(value[i]);
             }
           })
           break;
         }
         case 1: {
-          this.DisplayTask.forEach((task, i) => {
-            result = moment(this.DisplayTask[i].date).isAfter(this.todayDate);
-            if (result == true) {
-              this.resultArray.push(this.DisplayTask[i]);
+          value.forEach((task, i) => {
+            if (moment(value[i].date).isAfter(todayDate) == true) {
+              resultArray.push(value[i]);
             }
           })
           break;
         }
-        case 2: {
-          this.resultArray = this.DisplayTask;
+        default: {
+          resultArray = value;
         }
       }
+      return resultArray;
     }
-
-
-    deleteTask(taskId) {
-      let index = this.DisplayTask.findIndex(taskObj => taskObj.taskId == taskId);
-      this.DisplayTask.splice(index, 1);
-      localStorage.setItem("tasklist", JSON.stringify(this.DisplayTask));
-      /*  this.displayDefault(); */
-      this.filterTask(this.filterCriteria);
-    }
-
+    return filterTask();
   }
-   
-    return null;
-  }
+
+
+}
 
 
